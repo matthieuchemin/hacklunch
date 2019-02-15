6b9b36dfca7e40868e5755d9db224459d6e4f44b
NICOLAS TRUTET <nicolas@pepper.com>
2019-02-15 14:11:25 +0100

    init

---
+++ js/map.js
@@ -0,0 +1,40 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoicGVwcGVyaGFja2x1bmNoIiwiYSI6ImNqczV5ZXkyMzBpeHIzeXNieG0xc2J0dG4ifQ.pOCQjUyCYIzE4to5kjmEqw';
+						
+var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/mapbox/outdoors-v9', //stylesheet location
+    center: [13.4020788, 52.5270968], // starting position. // 52.5270968,13.4020788,15z
+    zoom: 13 // starting zoom
+});
+
+map.addControl(new mapboxgl.FullscreenControl());
+
+map.on('load', function() {
+    map.loadImage('resources/map-marker.png', function(error, image) {
+        if (error) throw error;
+        map.addImage('cat', image);
+        map.addLayer({
+            "id": "points",
+            "type": "symbol",
+            "source": {
+                "type": "geojson",
+                "data": {
+                    "type": "FeatureCollection",
+                    "features": [{
+                        "type": "Feature",
+                        "geometry": {
+                            "type": "Point",
+                            "coordinates": [13.4020788, 52.5270968]
+                        }
+                    }]
+                }
+            },
+            "layout": {
+                "icon-image": "cat",
+                "icon-size": 1
+            }
+        });
+    });
+});
+
+console.log("hfdfj")
